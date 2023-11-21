18082e5928b92994015f0ee9f15c755737f1c6cf
Joseph Marko <josephmmakaro@gmail.com>
2023-11-21 10:27:18 +0000

    FizzBuzz Bug fixes

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -17,12 +17,12 @@ def fizzbuzz(n):
 
     tmp_result = []
     for i in range(1, n + 1):
-        if (i % 3) == 0:
-            tmp_result.append("Fizz")
+        if (i % 5) == 0:
+            tmp_result.append("Buzz")
         elif (i % 3) == 0 and (i % 5) == 0:
             tmp_result.append("FizzBuzz")
-        elif (i % 5) == 0:
-            tmp_result.append("Buzz")
+        elif (i % 3) == 0:
+            tmp_result.append("Fizz")
         else:
             tmp_result.append(str(i))
     print(" ".join(tmp_result))
