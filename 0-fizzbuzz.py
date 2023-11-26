30ab43dad9108cfb3ea75e654f21784da7c10334
ifiok1equere <equereifiok1@gmail.com>
2023-11-26 10:10:57 +0100

    task 0: FizzBuzz

--- 0-fizzbuzz.py
+++ 0-fizzbuzz.py
@@ -17,10 +17,10 @@ def fizzbuzz(n):
 
     tmp_result = []
     for i in range(1, n + 1):
-        if (i % 3) == 0:
-            tmp_result.append("Fizz")
-        elif (i % 3) == 0 and (i % 5) == 0:
+        if (i % 3) == 0 and (i % 5) == 0:
             tmp_result.append("FizzBuzz")
+        elif (i % 3) == 0:
+            tmp_result.append("Fizz")
         elif (i % 5) == 0:
             tmp_result.append("Buzz")
         else:
